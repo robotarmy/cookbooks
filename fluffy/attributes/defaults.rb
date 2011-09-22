16a3064f5ca3a4f0e3d239b5d1a684b899895d11
Curtis Schofield <o_o@Curtiss-MacBook-Pro.local>
2011-09-22 01:20:57 -0700

    fluffy

---
+++ fluffy/attributes/defaults.rb
@@ -0,0 +1,14 @@
+default.ram[:op]         = "/op"
+
+default.fluffy[:dir]     = "#{node[:ram][:op]}/fluffy"
+default.fluffy[:app]     = "#{node[:ram][:op]}/fluffy/app"
+default.fluffy[:npm]     = "#{node[:ram][:op]}/fluffy/.node_modules"
+default.fluffy[:npmrc]   = "#{node[:ram][:op]}/fluffy/.npmrc"
+default.fluffy[:sv]      = "#{node[:fluffy][:dir]}/sv"
+
+default.fluffy[:config]  = "#{node[:fluffy][:app]}/lib/config.js"
+
+defaultfluffy][:user]    = "fluffy"
+defaultfluffy][:git]     = "git://github.com/RAM9/fluffy.git"
+
+
---
+++ fluffy/recipes/default.rb
@@ -0,0 +1,77 @@
+#
+# Cookbook Name:: fluffy
+# Recipe:: default
+#
+# Copyright 2010, RAM9, Curtis Schofield 
+#
+# All rights reserved - Redistribute at your own peril
+#
+#
+include_recipe "nodejs::npm"
+
+
+user node[:fluffy][:user] do
+  comment "fluffy daemon"
+	home node[:fluffy][:dir]
+  shell "/bin/zsh"
+	not_if 'id fluffy'
+end
+
+file "#{node[:fluffy][:dir]}/.zshrc" do
+  action :create
+end
+
+[node[:fluffy][:npm]].each do |dir| 
+	directory dir do
+		owner node[:fluffy][:user]
+		group node[:fluffy][:user]
+		mode 0755
+		recursive true
+	end
+end
+
+git "Sync Master fluffy" do
+	destination node[:fluffy][:app]
+  repository node[:fluffy][:git]
+	reference "master"
+	user node[:fluffy][:user]
+	group node[:fluffy][:user]
+  action :sync
+end
+
+template node[:fluffy][:npmrc] do
+  source "dot.npmrc.erb"
+  owner node[:fluffy][:user]
+  group node[:fluffy][:user]
+end
+
+template node[:fluffy][:config] do
+  source "config.js.erb"
+  owner node[:fluffy][:user]
+  group node[:fluffy][:user]
+end
+
+bash "install fluffy nodejs dependencies" do
+	# run as user - in login shell 
+	# so npm will read .npmrc
+	code <<-EOH
+	su - #{node[:fluffy][:user]} -c '
+  npm install node-http-proxy
+	EOH
+end
+
+
+daemontools_service "fluffy" do
+  template "fluffy"
+  directory node[:fluffy][:sv]
+  owner node[:fluffy][:user]
+  group node[:fluffy][:user]
+  log true 
+	env 'NODE_PATH' => node[:fluffy][:npm]
+  supports :start   => true,
+           :stop    => true,
+           :restart => true
+  action [:kill]
+end
+
+
